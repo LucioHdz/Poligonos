444681a307ec41875bbcbe2ec53e2173eda67ba2
LucioHdz <luciohdz0@gmail.com>
2021-06-01 12:17:40 -0500

    Completando Algoritmo bresenham

--- Algorithms.py
+++ Algorithms.py
@@ -54,16 +54,60 @@ def bresenham_algrithm(x,y,x2,y2):
     p = 2 * dy -dx
     x_vars=[]
     y_vars=[]
-    while x <= x2:
-        x_vars.append(x)
-        y_vars.append(y)   
-        x += 1
-        if p<0:
-            p = p + 2 * dy
+    if dx !=0:
+        if x<x2:
+            while x <= x2:
+                x_vars.append(x)
+                y_vars.append(y)   
+                x += 1
+                if p<0:
+                    p = p + 2 * dy
+                else:
+                    p = p + (2 * dy) - (2 * dx)
+                    if y< y2:
+                        y += 1
+                    elif y>y2:
+                        y -= 1
         else:
-            p = p + (2 * dy) - (2 * dx)
-            y += 1
+            while x >= x2:
+                x_vars.append(x)
+                y_vars.append(y)   
+                x -= 1
+                if p<0:
+                    p = p + 2 * dy
+                else:
+                    p = p + (2 * dy) - (2 * dx)
+                    if y< y2:
+                        y += 1
+                    elif y>y2:
+                        y -= 1
+    else:
+        if y<=y2:
+            while y <= y2:
+                x_vars.append(x)
+                y_vars.append(y)
+                if p<0:
+                    p = p + 2 * dy
+                else:
+                    p = p + (2 * dy) - (2 * dx)
+                    y += 1
+        else:
+            while y >= y2:
+                x_vars.append(x)
+                y_vars.append(y)
+                if p<0:
+                    p = p + 2 * dy
+                else:
+                    p = p + (2 * dy) - (2 * dx)
+                    y -= 1
     return x_vars,y_vars
 
 if __name__ == '__main__':
     print(bresenham_algrithm(0,0,10,0))
+    print(bresenham_algrithm(10,0,17,7))
+    print(bresenham_algrithm(17,7,17,17))
+    print(bresenham_algrithm(17,17,9,24))
+    print(bresenham_algrithm(9,24,0,24))
+    print(bresenham_algrithm(0,24,-7,17))
+    print(bresenham_algrithm(-7,17,-7,7))
+    print(bresenham_algrithm(-7,7,0,0))
